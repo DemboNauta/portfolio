4e8f5d7e6f9c560273ba41816391c64786318d94
Edgar Mila <edgarmila_10@outlook.com>
2024-07-15 23:36:12 +0200

    Añadido CV y mejorado vista en moviles

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+window.addEventListener('DOMContentLoaded', ()=>{
+    const navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach(link => {
+        link.classList.remove('activo');
+        link.addEventListener('click', ()=>{
+            if (window.innerWidth < 576) {
+                const menuToggle = document.getElementById('menuButton');
+                const bsCollapse = new bootstrap.Collapse(document.getElementById('navbarNav'), {toggle: false});
+                bsCollapse.toggle();
+            }
+        })
+    }); 
+})
 window.addEventListener('scroll', ()=>{
     let currentSection = '';
     const sections = document.querySelectorAll('section');
@@ -10,29 +23,40 @@ window.addEventListener('scroll', ()=>{
             currentSection = section.getAttribute('id');
         }
     });
-
-    navLinks.forEach(link => {
-        link.classList.remove('activo');
-        if (link.getAttribute('href').substring(2) === currentSection) {
-            link.classList.add('activo');
-        }
-    });
+    if(window.innerWidth>576){
+        navLinks.forEach(link => {
+            link.classList.remove('activo');
+            if (link.getAttribute('href').substring(2) === currentSection) {
+                link.classList.add('activo');
+            }
+        });
+    }else{
+        navLinks.forEach(link => {
+            link.classList.remove('activo');
+        });
+    }
 });
 
 document.addEventListener("DOMContentLoaded", ()=>{
+    
     document.querySelectorAll('.nav-link').forEach(function(anchor) {
         anchor.addEventListener('click', function(event) {
             event.preventDefault();
             let targetName = this.getAttribute('href').substr(1);
-            console.log(targetName);
             var targetElement = document.querySelector(targetName);
-            console.log(targetElement);
             var offset = targetElement.getBoundingClientRect().top + window.scrollY;
-
-            window.scrollTo({
-                top: (offset-40) - document.getElementById('header').offsetHeight,
-                behavior: 'smooth'
-            });
+            if(window.innerWidth>576){
+                window.scrollTo({
+                    top: (offset-40) - document.getElementById('header').offsetHeight,
+                    behavior: 'smooth'
+                });
+            }else{
+                window.scrollTo({
+                    top: (offset+100) - document.getElementById('header').offsetHeight,
+                    behavior: 'smooth'
+                });
+            }
+            
         });
     });
     
