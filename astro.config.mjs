81d0dae7aa923d71acd64776831e44144be97309
Edgar Milá <edgarmila_10@outlook.com>
2025-07-20 12:21:59 +0200

    Modificado forma de tratar los idiomas

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "es",
-    routing: { prefixDefaultLocale: true },
+    routing: { prefixDefaultLocale: false },
   },
   integrations: [tailwind()],
 });
